7d20fb3b1fbc86fa90766d1f62eba8007f29ff35
Koichi ITO <koic.ito@gmail.com>
2016-01-07 13:44:24 +0900

    Put git branch in shell prompt

--- dot.bash/git.bash
+++ dot.bash/git.bash
@@ -1,1 +1,8 @@
 alias g='git'
+
+GIT_VERSION=2.4.4
+source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-prompt.sh
+source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-completion.bash
+
+GIT_PS1_SHOWDIRTYSTATE=true
+export PS1='\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '

7c8be1a233a1846ab601817d4e4453406916774b
Koichi ITO <koic.ito@gmail.com>
2016-02-15 21:27:25 +0900

    It slipped commands for the Rails development

---
+++ dot.bash/rails.sh
@@ -0,0 +1,7 @@
+# Testing for Active Record with MySQL
+alias ar_rake_db_create_mysql='bundle exec rake mysql:build_databases'
+alias ar_rake_test_mysql='bundle rake test_mysql'
+
+# Testing for Active Record with PostgreSQL
+alias ar_rake_db_create_postgresql='bundle exec rake postgresql:build_databases'
+alias ar_rake_test_postgresql='bundle exec rake test_postgresql'

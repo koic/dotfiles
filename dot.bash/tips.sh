f15c8547e9eea7d2d94de380b4ff21062d7a066e
Koichi ITO <koic.ito@gmail.com>
2016-02-18 14:55:34 +0900

    Insert a magic comment that frozen string literal

--- dot.bash/tips.sh
+++ dot.bash/tips.sh
@@ -7,3 +7,5 @@ function replace_text() {
 
   bash -c "ack -l $1 | xargs sed -i -e \"s/$1/$2/g\""
 }
+
+alias insert_frozen_string_literal_of_magic_comment='find . -name *.rb | xargs sed -i -e "1i# -\*- frozen-string-literal: true -\*-\n"'

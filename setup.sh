d93d9f4464fce792469d926a01801fe65c50385e
Koichi ITO <koic.ito@gmail.com>
2016-03-10 11:27:07 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DOTFILES_SRC=$HOME/work/git/dotfiles
+
+ln -sf $DOTFILES_SRC/dot.bash $HOME/.bash
+ln -sf $DOTFILES_SRC/dot.gemrc $HOME/.gemrc
+ln -sf $DOTFILES_SRC/dot.gitignore $HOME/.gitignore
+ln -sf $DOTFILES_SRC/dot.peco $HOME/.peco
+ln -sf $DOTFILES_SRC/dot.screenrc $HOME/.screenrc
+ln -sf $DOTFILES_SRC/dot.bashrc $HOME/.bashrc
+ln -sf $DOTFILES_SRC/dot.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_SRC/dot.irbrc $HOME/.irbrc
+ln -sf $DOTFILES_SRC/dot.pryrc $HOME/.pryrc

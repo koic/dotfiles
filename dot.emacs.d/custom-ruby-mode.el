9d049740af896466e02e860d738a24ce6fa82e91
Koichi ITO <koic.ito@gmail.com>
2016-04-07 01:15:45 +0900

    Customize ruby-mode
    Don't need magic encoding comment.

---
+++ dot.emacs.d/custom-ruby-mode.el
@@ -0,0 +1,4 @@
+;; Ignore what is going on with magic encoding comment written by ruby-mode
+;;   Thx http://qiita.com/ongaeshi/items/5b2544d81146a000e638
+(custom-set-variables
+  '(ruby-insert-encoding-magic-comment nil))

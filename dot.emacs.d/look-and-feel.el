f2af4cd716fbc64aa66af3f1b2f8fe244dc11ffb
Koichi ITO <koic.ito@gmail.com>
2016-04-07 01:51:50 +0900

    Look and feel settings for Emacs

---
+++ dot.emacs.d/look-and-feel.el
@@ -0,0 +1,13 @@
+;; Enable visual line mode
+;;   Thx http://alainmathematics.blogspot.jp/2013/07/emacs.html
+(global-visual-line-mode)
+
+;; Highlight cursor line
+(global-hl-line-mode t)
+
+(custom-set-faces
+  '(hl-line ((t (:background "MistyRose1"))))
+)
+
+;; Disable menu bar
+(menu-bar-mode -1)

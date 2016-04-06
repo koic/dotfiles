066cfdd4a176fb9d2352c73faee26ddcfb4373b3
Koichi ITO <koic.ito@gmail.com>
2016-04-07 02:03:49 +0900

    For Oracle client setting

--- dot.bash/oracle.sh
+++ dot.bash/oracle.sh
@@ -3,6 +3,7 @@ topdir="${HOME}/opt/oracle"
 bindir="${topdir}/bin"
 libdir="${topdir}/lib"
 
+export NLS_LANG=Japanese_Japan.AL32UTF8
 export ORACLE_HOME=${topdir}
 export PATH=${ORACLE_HOME}/bin:$PATH
 export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib

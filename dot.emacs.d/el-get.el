2ed8b0453aaa9fe656f0344ff3705152bc047096
Koichi ITO <koic.ito@gmail.com>
2016-04-11 22:10:30 +0900

    Introduce El-Get to Emacs
    Thanks: http://tarao.hatenablog.com/entry/20150221/1424518030

---
+++ dot.emacs.d/el-get.el
@@ -0,0 +1,10 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+    (url-retrieve-synchronously
+      "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+      (eval-print-last-sexp)))

1f406b16afab56a5c65ff9d31ab02e5c77491d02
Koichi ITO <koic.ito@gmail.com>
2016-04-13 21:57:58 +0900

    Inline an elisp file

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,3 +1,14 @@
-(load "~/.emacs.d/el-get.el")
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+    (url-retrieve-synchronously
+      "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+      (eval-print-last-sexp)))
+
+(load "~/.emacs.d/el-get-bundle.el")
 
 (mapc 'load (file-expand-wildcards "~/.emacs.d/lisp/*.el"))

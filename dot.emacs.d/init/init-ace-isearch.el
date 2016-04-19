38fd112be4694a382f2a012355112b5a53b774a1
Koichi ITO <koic.ito@gmail.com>
2016-04-19 20:29:30 +0900

    Remove a needless codes

--- dot.emacs.d/init/init-ace-isearch.el
+++ dot.emacs.d/init/init-ace-isearch.el
@@ -1,8 +1,6 @@
 ;; There's dirty hack due to couldn't load these files.
-(add-to-list 'load-path (locate-user-emacs-file "elpa/ace-isearch-20150808.556"))
 (add-to-list 'load-path (locate-user-emacs-file "elpa/helm-swoop-20160417.1457"))
 (add-to-list 'load-path (locate-user-emacs-file "elpa/ace-jump-mode-20140616.115"))
 (add-to-list 'load-path (locate-user-emacs-file "elpa/avy-20160414.415"))
 
-(require 'ace-isearch)
 (global-ace-isearch-mode +1)

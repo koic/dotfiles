5e7030813d5fa3029802158e1055fabb1e1203c1
Koichi ITO <koic.ito@gmail.com>
2016-04-19 20:29:06 +0900

    Change to useful setting

--- dot.emacs.d/init/init-ace-isearch.el
+++ dot.emacs.d/init/init-ace-isearch.el
@@ -6,5 +6,3 @@
 
 (require 'ace-isearch)
 (global-ace-isearch-mode +1)
-(setq ace-isearch-use-function-from-isearch nil)
-(define-key isearch-mode-map (kbd "M-o") 'helm-multi-swoop-all-from-isearch)

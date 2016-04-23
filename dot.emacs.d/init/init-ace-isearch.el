938d6fe2fc15f0e85a3c7fc7cff548ca9102f5e2
Koichi ITO <koic.ito@gmail.com>
2016-04-23 11:51:20 +0900

    :nail_care:

--- dot.emacs.d/init/init-ace-isearch.el
+++ dot.emacs.d/init/init-ace-isearch.el
@@ -1,6 +1,6 @@
 ;; There's dirty hack due to couldn't load these files.
-(add-to-list 'load-path (locate-user-emacs-file "elpa/helm-swoop-20160417.1457"))
-(add-to-list 'load-path (locate-user-emacs-file "elpa/ace-jump-mode-20140616.115"))
-(add-to-list 'load-path (locate-user-emacs-file "elpa/avy-20160414.415"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/helm-swoop"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/ace-jump-mode"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/avy"))
 
 (global-ace-isearch-mode +1)

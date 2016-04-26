606b9e8308dd50f404333b3f640cfd76e721c0ec
Koichi ITO <koic.ito@gmail.com>
2016-04-26 12:31:53 +0900

    Suppress "Wrote ~/.emacs.d/.recentf" message
    Thanks to http://keisanbutsuriya.hateblo.jp/entry/2015/02/15/174758

--- dot.emacs.d/init/init-recentf.el
+++ dot.emacs.d/init/init-recentf.el
@@ -1,6 +1,13 @@
+(defmacro with-suppressed-message (&rest body)
+  "Suppress new messages temporarily in the echo area and the `*Messages*' buffer while BODY is evaluated."
+  (declare (indent 0))
+  (let ((message-log-max nil))
+    `(with-temp-message (or (current-message) "") ,@body)))
+
 (require 'recentf)
 (setq recentf-save-file (locate-user-emacs-file ".recentf"))
-(setq recentf-max-saved-items 1000)            ;; Count of stored file in recentf
-(setq recentf-exclude '(".recentf"))           ;; Ignore .recentf-self
-(setq recentf-auto-cleanup 10)                 ;; Auto cleanup
-(run-with-idle-timer 30 t 'recentf-save-list)  ;; Store .recentf file every 30 secounds
+(setq recentf-max-saved-items 1000)               ;; Count of stored file in recentf
+(setq recentf-exclude '(".recentf"))              ;; Ignore .recentf-self
+(setq recentf-auto-cleanup 10)                    ;; Auto cleanup
+(run-with-idle-timer 30 t '(lambda ()             ;; Store .recentf file every 30 secounds
+  (with-suppressed-message (recentf-save-list))))

3e126bc82d347429dc62393a622aaeccc1bc9cd8
Koichi ITO <koic.ito@gmail.com>
2016-04-28 00:00:38 +0900

    Introduce contrib/diff-highlight

--- dot.bash/git.sh
+++ dot.bash/git.sh
@@ -1,6 +1,9 @@
 alias g='git'
 
 GIT_VERSION=`git --version | cut -d ' ' -f3`
+
+PATH=$PATH:/usr/local/Cellar/git/$GIT_VERSION/share/git-core/contrib/diff-highlight
+
 source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-prompt.sh
 source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-completion.bash
 

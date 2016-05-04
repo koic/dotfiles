b30c05c79580b33b4c6182fff3d151df482bc410
Koichi ITO <koic.ito@gmail.com>
2016-05-04 23:42:05 +0900

    Install zsh and oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ ln -sf $DOTFILES_SRC/dot.agignore $HOME/.agignore
 ln -sf $DOTFILES_SRC/dot.bash $HOME/.bash
 ln -sf $DOTFILES_SRC/dot.gemrc $HOME/.gemrc
 ln -sf $DOTFILES_SRC/dot.gitignore $HOME/.gitignore
+ln -sf $DOTFILES_SRC/dot.oh-my-zsh $HOME/.oh-my-zsh
 ln -sf $DOTFILES_SRC/dot.peco $HOME/.peco
 ln -sf $DOTFILES_SRC/dot.screenrc $HOME/.screenrc
 ln -sf $DOTFILES_SRC/dot.bashrc $HOME/.bashrc
 ln -sf $DOTFILES_SRC/dot.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_SRC/dot.irbrc $HOME/.irbrc
 ln -sf $DOTFILES_SRC/dot.pryrc $HOME/.pryrc
+ln -sf $DOTFILES_SRC/dot.zshrc $HOME/.zshrc

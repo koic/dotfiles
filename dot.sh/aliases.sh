2ec676b8a31dac40a68d58249200bf58245b0ead
Koichi ITO <koic.ito@gmail.com>
2016-05-05 04:25:13 +0900

    Add more laziness options :relaxed:
    Thanks to http://qiita.com/hatchinee/items/586fb1c4915e2bb5c03b

--- dot.sh/aliases.sh
+++ dot.sh/aliases.sh
@@ -1,4 +1,6 @@
 alias date='gdate' # Use GNU's date in Mac OS X
 alias tac='gtac'   # Use GNU's tac in Mac OS X
 
+alias less='less --no-init --quit-if-one-screen'
+
 alias ripper-tags='ripper-tags -e -R -f TAGS' # `gem install ripper-tags'

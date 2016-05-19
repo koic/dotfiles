58adabe2a16fa207d122c642811e967d24dc9ae3
Koichi ITO <koic.ito@gmail.com>
2016-05-19 10:50:21 +0900

    Improve argument that allow a specific file path

--- dot.sh/tips.sh
+++ dot.sh/tips.sh
@@ -1,11 +1,17 @@
 function replace_text() {
-  if [ $# -ne 2 ]; then
-    echo "usage: replace_text pattern replacement" 1>&2
+  local target
+
+  if [ $# -eq 2 ]; then
+    target="ag -l $1"
+  elif [ $# -eq 3 ]; then
+    target="ls $3"
+  else
+    echo "usage: replace_text pattern replacement [path]" 1>&2
 
     return 1
   fi
 
-  bash -c "ag -l $1 | xargs sed -i -e \"s/$1/$2/g\""
+  bash -c "$target | xargs sed -i -e \"s/$1/$2/g\""
 }
 
 alias insert_frozen_string_literal_of_magic_comment='find . -name *.rb | xargs sed -i -e "1i# -\*- frozen-string-literal: true -\*-\n"'

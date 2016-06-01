9cfaf5569fb4586013eeffd116157ec55c28dd91
Koichi ITO <koic.ito@gmail.com>
2016-06-01 17:32:11 +0900

    Support OS X El Capitan
    Accept a illegal version (ex. 10.10xxxx) is a known problem.

--- dot.sh/oracle.sh
+++ dot.sh/oracle.sh
@@ -6,4 +6,11 @@ libdir="${topdir}/lib"
 export NLS_LANG=Japanese_Japan.AL32UTF8
 export ORACLE_HOME=${topdir}
 export PATH=${ORACLE_HOME}/bin:$PATH
-export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib
+
+local OS_PRODUCT_VERSION=`sw_vers -productVersion`
+
+case $OS_PRODUCT_VERSION in
+  10.9*|10.10*)  export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib;; # for Mavericks, Yosemite
+  10.11*)        export OCI_DIR=${ORACLE_HOME}/lib;;           # for El Capitan
+  *)             echo "Version OS X $OS_PRODUCT_VERSION not supported ";;
+esac

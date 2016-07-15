ab7c190dab8abd920952fc94225b1c31d7c06658
Koichi ITO <koic.ito@gmail.com>
2016-07-15 16:04:49 +0900

    Add an aliase for parallel_tests

--- dot.sh/parallel_tests.sh
+++ dot.sh/parallel_tests.sh
@@ -1,4 +1,5 @@
 alias parallel_db_schema_load="bundle exec parallel_test --exec 'rake db:schema:load RAILS_ENV=test' -n 4"
 alias parallel_db_migrate_reset="bundle exec parallel_test --exec 'rake db:migrate:reset RAILS_ENV=test' -n 4"
 alias parallel_prepare="RAILS_ENV=test bundle exec rake 'parallel:prepare[4]'"
+alias parallel_spec_with_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb'"
 alias parallel_spec_with_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb|.feature'"

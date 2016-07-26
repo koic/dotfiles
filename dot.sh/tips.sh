92aa9eac3ea47686b66f2de97149a88dc4e6b9aa
Koichi ITO <koic.ito@gmail.com>
2016-07-26 13:29:07 +0900

    SlideShare causes problems to display Japanese font
    cf. http://qiita.com/yasulab/items/49511c27179be454d568#comment-ff8f2076c0aee4dca4af

--- dot.sh/tips.sh
+++ dot.sh/tips.sh
@@ -14,6 +14,16 @@ function replace_text() {
   bash -c "$target | xargs sed -i -e \"s/$1/$2/g\""
 }
 
+function convert_uploadable_keynote_pdf() {
+  if [ $# -eq 1 ]; then
+      bash -c "LANG=C LC_ALL=C sed -i 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $1"
+  else
+    echo "usage: convert_uploadable_keynote_pdf pdf_path" 1>&2
+
+    return 1
+  fi
+}
+
 alias insert_frozen_string_literal_of_magic_comment='find . -name *.rb | xargs sed -i -e "1i# -\*- frozen-string-literal: true -\*-\n"'
 
 alias rbenv_update='pushd . && cd ~/.rbenv && git pull origin master && cd ~/.rbenv/plugins/ruby-build && git pull origin master && popd'

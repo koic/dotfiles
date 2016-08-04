3453fa333220554821f530d7b0699b3f9e552f14
Koichi ITO <koic.ito@gmail.com>
2016-08-04 11:37:25 +0900

    Sharing clipborad with Emacs and Mac OS
    Thanks to https://yono05.wordpress.com/2011/11/20

---
+++ dot.emacs.d/lisp/clipborad.el
@@ -0,0 +1,21 @@
+;; system-type predicates
+;; from http://d.hatena.ne.jp/tomoya/20090807/1249601308
+(setq darwin-p   (eq system-type 'darwin)
+      linux-p    (eq system-type 'gnu/linux)
+      carbon-p   (eq system-type 'mac)
+      meadow-p   (featurep 'meadow))
+
+; Sharing clipborad with Emacs and Mac OS
+; from http://hakurei-shain.blogspot.com/2010/05/mac.html
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(if (or darwin-p carbon-p)
+    (setq interprogram-cut-function 'paste-to-osx)
+    (setq interprogram-paste-function 'copy-from-osx))

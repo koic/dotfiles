389f9326739d07cd9c920fb75b70d044ca90bf9a
Koichi ITO <koic.ito@gmail.com>
2016-08-29 10:32:47 +0900

    Settings for individual testing

--- dot.sh/parallel_tests.sh
+++ dot.sh/parallel_tests.sh
@@ -1,5 +1,6 @@
 alias parallel_db_schema_load="bundle exec parallel_test --exec 'rake db:schema:load RAILS_ENV=test' -n 4"
 alias parallel_db_migrate_reset="bundle exec parallel_test --exec 'rake db:migrate:reset RAILS_ENV=test' -n 4"
 alias parallel_prepare="RAILS_ENV=test bundle exec rake 'parallel:prepare[4]'"
-alias parallel_spec_with_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb'"
+alias parallel_spec="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb'"
+alias parallel_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '.feature'"
 alias parallel_spec_with_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb|.feature'"

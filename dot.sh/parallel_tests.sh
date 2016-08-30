fd0472160bbcdc12d6aa10c9c66b3f6190f2e43d
Koichi ITO <koic.ito@gmail.com>
2016-08-30 16:15:27 +0900

    DRY

--- dot.sh/parallel_tests.sh
+++ dot.sh/parallel_tests.sh
@@ -1,6 +1,16 @@
+function parallel_rspec_rails_cuke() {
+  if [ $# -eq 1 ]; then
+    bash -c "RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '$1'"
+  else
+    echo "usage: parallel_rspec_rails_cuke path" 1>&2
+
+    return 1
+  fi
+}
+
 alias parallel_db_schema_load="bundle exec parallel_test --exec 'rake db:schema:load RAILS_ENV=test' -n 4"
 alias parallel_db_migrate_reset="bundle exec parallel_test --exec 'rake db:migrate:reset RAILS_ENV=test' -n 4"
 alias parallel_prepare="RAILS_ENV=test bundle exec rake 'parallel:prepare[4]'"
-alias parallel_spec="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb'"
-alias parallel_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '.feature'"
-alias parallel_spec_with_cuke="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb|.feature'"
+alias parallel_spec="parallel_rspec_rails_cuke '_spec.rb'"
+alias parallel_cuke="parallel_rspec_rails_cuke '.feature'"
+alias parallel_spec_with_cuke="parallel_rspec_rails_cuke '_spec.rb|.feature'"

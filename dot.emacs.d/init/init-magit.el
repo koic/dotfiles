9989d5fff642884772971ad8d626452fbad093dc
Koichi ITO <koic.ito@gmail.com>
2016-08-31 11:57:24 +0900

    Disable git-rebase-mode
    Thanks http://qiita.com/hash/items/bbd903fdfb954ee1c175

--- dot.emacs.d/init/init-magit.el
+++ dot.emacs.d/init/init-magit.el
@@ -2,3 +2,6 @@
 (add-to-list 'load-path (locate-user-emacs-file "el-get/magit/lisp"))
 (load (locate-user-emacs-file "el-get/magit/lisp/git-commit.elc"))
 (require 'magit)
+
+;; Disable git-rebase-mode
+(setq auto-mode-alist (remove (rassoc 'git-rebase-mode auto-mode-alist) auto-mode-alist))

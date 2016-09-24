107966297dc8ea81ec7c7205aba5dbcb3d213cdf
Koichi ITO <koic.ito@gmail.com>
2016-09-24 10:44:53 +0900

    It make parallel execution with 4 processes :dash: :dash: :dash:
    Thanks http://www.slideshare.net/hsbt/practical-testing-of-ruby-core/23

--- dot.sh/ruby.sh
+++ dot.sh/ruby.sh
@@ -1,2 +1,3 @@
 alias configure_ruby='autoconf && ./configure --disable-install-doc --with-openssl-dir=`brew --prefix openssl`'
 alias make_check_ruby='make clean && make -j && make check'
+alias make_ruby_test='make test-all TESTS="-j4"'

9ac0bc296881ffe97117957fab3328c01d00f4f8
Junichi Kobayashi <junichi.kobayashi@esm.co.jp>
2016-09-30 13:58:46 +0900

    :golf:

--- dot.sh/gem-src.sh
+++ dot.sh/gem-src.sh
@@ -1,2 +1,2 @@
 alias install_gem-src='git clone https://github.com/amatsuda/gem-src.git ~/.rbenv/plugins/gem-src'
-alias update_gem-src='pushd . && cd ~/.rbenv/plugins/gem-src && git pull && popd'
+alias update_gem-src='pushd ~/.rbenv/plugins/gem-src && git pull && popd'

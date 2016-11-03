3a31d399e103990aeacf4cc2e073f8576d775b8a
Koichi ITO <koic.ito@gmail.com>
2016-11-03 16:19:39 +0900

    Add the suffix validation

--- dot.sh/tips.sh
+++ dot.sh/tips.sh
@@ -17,6 +17,14 @@ function out_diff() {
 }
 
 function convert_uploadable_keynote_pdf() {
+  extension=${1##*.}
+
+  if [ $extension != "pdf" ]; then
+    echo 'Suffix needs to "pdf"' 1>&2
+
+    return 1
+  fi
+
   if [ $# -eq 1 ]; then
       bash -c "LANG=C LC_ALL=C sed -i 's|/Registry (Adobe) /Ordering (Japan1) /Supplement [0-9]|/Registry(Adobe) /Ordering(Identity) /Supplement 0|g' $1"
   else

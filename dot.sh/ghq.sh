d53fd9cc86682d07b27644c1dcc2699bf80bec75
Koichi ITO <koic.ito@gmail.com>
2016-12-15 12:35:30 +0900

    Accept initial value for peco

--- dot.sh/ghq.sh
+++ dot.sh/ghq.sh
@@ -1,2 +1,15 @@
-alias ghqlist='cd $(ghq list -p | peco)'
-alias ghqhub='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
+function ghqlist() {
+    if [ $# -eq 1 ]; then
+        cd $(ghq list -p | peco --query $1)
+    else
+        cd $(ghq list -p | peco)
+    fi
+}
+
+function ghqhub() {
+    if [ $# -eq 1 ]; then
+        hub browse $(ghq list | peco --query $1 | cut -d "/" -f 2,3)
+    else
+        hub browse $(ghq list | peco | cut -d "/" -f 2,3)
+    fi
+}

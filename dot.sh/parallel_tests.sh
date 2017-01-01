ca758ee056003c0472a454edcfd36bd807825def
Koichi ITO <koic.ito@gmail.com>
2017-01-01 22:08:04 +0900

    Use `rails` command instead of `rake` command

--- dot.sh/parallel_tests.sh
+++ dot.sh/parallel_tests.sh
@@ -9,9 +9,9 @@ function parallel_rspec_rails_cuke() {
 }
 
 alias parallel_db_environment_set="bundle exec parallel_test --exec 'rails db:environment:set RAILS_ENV=test' -n 4"
-alias parallel_db_schema_load="bundle exec parallel_test --exec 'rake db:schema:load RAILS_ENV=test' -n 4"
-alias parallel_db_migrate_reset="bundle exec parallel_test --exec 'rake db:migrate:reset RAILS_ENV=test' -n 4"
-alias parallel_prepare="RAILS_ENV=test bundle exec rake 'parallel:prepare[4]'"
+alias parallel_db_schema_load="bundle exec parallel_test --exec 'rails db:schema:load RAILS_ENV=test' -n 4"
+alias parallel_db_migrate_reset="bundle exec parallel_test --exec 'rails db:migrate:reset RAILS_ENV=test' -n 4"
+alias parallel_prepare="RAILS_ENV=test bundle exec rails 'parallel:prepare[4]'"
 alias parallel_spec="parallel_rspec_rails_cuke '_spec.rb'"
 alias parallel_cuke="parallel_rspec_rails_cuke '.feature'"
 alias parallel_spec_with_cuke="parallel_rspec_rails_cuke '_spec.rb|.feature'"

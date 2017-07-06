bf02d1c39a723089882e2f2ea581d192a8b46794
Koichi ITO <koic.ito@gmail.com>
2017-07-06 11:16:21 +0900

    Add an alias that install the dev version of Bundler

--- dot.sh/bundler.sh
+++ dot.sh/bundler.sh
@@ -1,3 +1,5 @@
 alias be='bundle exec'
 alias bi='bundle install'
 alias bu='bundle update'
+
+alias install_bundler_dev="cd `ghq root`/github.com/bundler/bundler && git pull && rake install && \cd -"

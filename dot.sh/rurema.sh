3d4c15b5c7583f7ccc4800cbf7580bb572c35d2a
Koichi ITO <koic.ito@gmail.com>
2018-01-08 00:24:46 +0900

    Support Ruby 2.5.0

--- dot.sh/rurema.sh
+++ dot.sh/rurema.sh
@@ -9,7 +9,7 @@
 #  cd `ghq root`/github.com/rurema/doctree && rake generate
 #
 function rurema_server_setup() {
-    for version in 1.8.7 1.9.3 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0
+    for version in 1.8.7 1.9.3 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0 2.5.0
     do
         ln -sfn /tmp/db-$version `ghq root`/github.com/rurema/bitclust/db-$version
     done

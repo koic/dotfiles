30655ef33327b00ffe385c063876f5b05d39050f
Koichi ITO <koic.ito@gmail.com>
2018-09-23 09:52:40 +0900

    Update gem-src's setup
    Use `ghq get` style instead of `git clone` style.

--- dot.sh/gem-src.sh
+++ dot.sh/gem-src.sh
@@ -1,2 +1,9 @@
-alias install_gem-src='git clone https://github.com/amatsuda/gem-src.git ~/.rbenv/plugins/gem-src'
-alias update_gem-src='pushd ~/.rbenv/plugins/gem-src && git pull && popd'
+GEM_SRC_ORIGIN_PATH=`ghq root`/github.com/amatsuda/gem-src
+if [ ! -e $GEM_SRC_ORIGIN_PATH ]; then
+    ghq get https://github.com/amatsuda/gem-src
+fi
+
+GEM_SRC_SYMLINK_PATH=$HOME/.rbenv/plugins/gem-src
+if [ ! -e $GEM_SRC_SYMLINK_PATH ]; then
+    ln -sf $GEM_SRC_ORIGIN_PATH $HOME/.rbenv/plugins/gem-src
+fi

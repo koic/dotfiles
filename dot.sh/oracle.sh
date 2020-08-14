983ad8677ace6cfd80b1c3f8e6cc607833542b1d
Koichi ITO <koic.ito@gmail.com>
2020-08-15 05:33:26 +0900

    Support macOS Mojave

--- dot.sh/oracle.sh
+++ dot.sh/oracle.sh
@@ -10,9 +10,9 @@ export PATH=${ORACLE_HOME}/bin:$PATH
 local OS_PRODUCT_VERSION=`sw_vers -productVersion`
 
 case $OS_PRODUCT_VERSION in
-    10.9*|10.10*)         export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib;; # for Mavericks, Yosemite
-    10.11*|10.12*|10.13*) export OCI_DIR=${ORACLE_HOME}/lib;;           # for El Capitan, Sierra, High Sieera
-    *)                    echo "Version OS X $OS_PRODUCT_VERSION not supported.";;
+    10.9*|10.10*)                export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib;; # for Mavericks, Yosemite
+    10.11*|10.12*|10.13*|10.14*) export OCI_DIR=${ORACLE_HOME}/lib;;           # for El Capitan, Sierra, High Sieera, Mojave
+    *)                           echo "Version OS X $OS_PRODUCT_VERSION not supported.";;
 esac
 
 alias sqlplus='rlwrap sqlplus'

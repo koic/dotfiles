e60cacac22b2bd9ca28da7a6d959ad469f6b37b5
Koichi ITO <koic.ito@gmail.com>
2020-12-23 16:25:52 +0900

    Support macOS Catalina

--- dot.sh/oracle.sh
+++ dot.sh/oracle.sh
@@ -5,12 +5,14 @@ libdir="${topdir}/lib"
 
 export NLS_LANG=Japanese_Japan.AL32UTF8
 export ORACLE_HOME=${topdir}
+export OCI_DIR=$(brew --prefix)/lib
 export PATH=${ORACLE_HOME}/bin:$PATH
 
 local OS_PRODUCT_VERSION=`sw_vers -productVersion`
 
+export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib
 case $OS_PRODUCT_VERSION in
-    10.9*|10.10*)                export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib;; # for Mavericks, Yosemite
+    10.9*|10.10*|10.15*)         export DYLD_LIBRARY_PATH=${ORACLE_HOME}/lib;; # for Mavericks, Yosemite, Catalina
     10.11*|10.12*|10.13*|10.14*) export OCI_DIR=${ORACLE_HOME}/lib;;           # for El Capitan, Sierra, High Sieera, Mojave
     *)                           echo "Version OS X $OS_PRODUCT_VERSION not supported.";;
 esac

358e157bad5c1a71c2c33970434b1a8c30ee5cd0
Koichi ITO <koic.ito@gmail.com>
2021-02-03 15:49:55 +0900

    Sort by repository path

--- dot.sh/ghq.sh
+++ dot.sh/ghq.sh
@@ -1,8 +1,8 @@
 function cr() {
     if [ $# -eq 1 ]; then
-        repo=$(ghq list -p | peco --query $1)
+        repo=$(ghq list -p | sort | peco --query $1)
     else
-        repo=$(ghq list -p | peco)
+        repo=$(ghq list -p | sort | peco)
     fi
 
     if [ -n "$repo" ]; then
@@ -12,16 +12,16 @@ function cr() {
 
 function ghqhub() {
     if [ $# -eq 1 ]; then
-        hub browse $(ghq list | peco --query $1 | cut -d "/" -f 2,3)
+        hub browse $(ghq list | sort | peco --query $1 | cut -d "/" -f 2,3)
     else
-        hub browse $(ghq list | peco | cut -d "/" -f 2,3)
+        hub browse $(ghq list | sort | peco | cut -d "/" -f 2,3)
     fi
 }
 
 function ghqgem() {
     if [ $# -eq 1 ]; then
-        open "https://rubygems.org/gems/$(ghq list | peco --query $1 | cut -d "/" -f 3)"
+        open "https://rubygems.org/gems/$(ghq list | sort | peco --query $1 | cut -d "/" -f 3)"
     else
-        open "https://rubygems.org/gems/$(ghq list | peco | cut -d "/" -f 3)"
+        open "https://rubygems.org/gems/$(ghq list | sort | peco | cut -d "/" -f 3)"
     fi
 }

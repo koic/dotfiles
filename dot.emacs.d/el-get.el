854556432c2f401a7de2e9624ee96e3a0238dc4c
Koichi ITO <koic.ito@gmail.com>
2022-03-04 23:13:12 +0900

    M-x el-get-install RET rust-mode

--- dot.emacs.d/el-get.el
+++ dot.emacs.d/el-get.el
@@ -30,6 +30,7 @@
 (el-get-bundle recentf-ext)
 (el-get-bundle rspec-mode)
 (el-get-bundle ruby-end)
+(el-get-bundle rust-mode)
 (el-get-bundle slim-mode)
 (el-get-bundle sync-recentf)
 (el-get-bundle undo-tree)

81bf550cca566c2f7322894f0b8e4f1e522063ac
Koichi ITO <koic.ito@gmail.com>
2022-11-10 17:17:19 +0900

    Remove unused code

--- dot.sh/git.sh
+++ dot.sh/git.sh
@@ -9,9 +9,5 @@ PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight
 
 source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-prompt.sh
 
-if [ `echo $SHELL | grep -c "zsh"` -eq 1 ]; then
-    source /usr/local/Cellar/git/$GIT_VERSION/etc/bash_completion.d/git-completion.zsh
-fi
-
 GIT_PS1_SHOWDIRTYSTATE=true
 export PS1='\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
